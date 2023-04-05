bf5ad2a7cc44161c1f2c75180255f6fd393c8a1c
IDGS-802-20001500 <mauricioisraelfer@gmail.com>
2023-04-05 01:20:00 -0600

    Avance de módulo de inventario 1/3, 05/04/2023, Se creo la interfaz y hoja de estilos al modulo de inventario, se tiene un pequeño atrazo por vinculación de 2 tablas en la BD

--- project/main.py
+++ project/main.py
@@ -2,6 +2,7 @@ from flask import Blueprint,render_template
 from flask_security import login_required,current_user
 from flask_security.decorators import roles_required,roles_accepted
 from . import db
+from .models import Inventario, Producto, MateriaPrima
 
 main=Blueprint('main', __name__)
 
@@ -18,3 +19,11 @@ def index():
 def profile():
     return render_template('profile.html', name=current_user.name)
 
+@main.route('/inventario')
+@login_required
+@roles_accepted('Administrador')
+def inventario():
+
+    inv = Inventario.query.all()
+
+    return render_template("inventario.html", inv=inv)
